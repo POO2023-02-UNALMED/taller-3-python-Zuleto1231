f91a2d903c05a1240bedfd0f9b3d1740d69ddf65
Zuleto1231 <mzueta@unal.edu.co>
2023-08-26 20:46:21 -0500

    Update tv.py

--- televisores/tv.py
+++ televisores/tv.py
@@ -24,14 +24,14 @@ class TV:
         return self.canal
     
     def setCanal(self,canal):
-        if 1<=canal<=120:
+        if self._estado and canal <= 120 and canal >= 1:
             self.canal=canal
             
     def getVolumen(self):
         return self.volumen
     
     def setVolumen(self,volumen):
-        if 0<=volumen<=7:
+        if self._estado and volumen >=0 and volumen <=7:
             self.volumen=volumen
             
     def getControl(self):
